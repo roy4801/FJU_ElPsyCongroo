a9d1cf2946429d93a6ff83fa54816eabfa2ae1b3
roy4801 <a82611141@gmail.com>
2018-09-12 16:56:12 +0800

    Sieve of Eratosthenes - Prime Table worked

---
+++ codebook/prime/sieve_prime.cpp
@@ -0,0 +1,22 @@
+/*
+ * Sieve of Eratosthenes Test
+ * Author: roy4801
+ * Team: FJU_ElPsyCongroo
+ */
+#include "sieve.h"
+
+using namespace std;
+
+int main()
+{
+	buildPrimeTable();
+
+	// Primes below 1000
+	for(int i = 1; i <= 1000; i++)
+	{
+		if(prime[i])
+			printf("%d\n", i);
+	}
+
+	return 0;
+}
