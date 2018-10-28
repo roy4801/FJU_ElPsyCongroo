f7aa025aa30ee014b67cd5c3fefda122596623b2
roy4801 <a82611141@gmail.com>
2018-10-28 14:16:45 +0800

    [codebook] segment tree refactored

--- codebook/Sequence/RMQ/segmentTree_test.cpp
+++ codebook/Sequence/RMQ/segmentTree_test.cpp
@@ -1,4 +1,5 @@
 #include "segmentTree.h"
+#include <cmath>
 
 using namespace std;
 
@@ -7,23 +8,40 @@ using namespace std;
 
 #define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
 
+int pow(int n)
+{
+	if(n == 0)
+		return 1;
+	else if(n == 1)
+		return 2;
+
+	int half = pow(n/2);
+
+	if(n % 2 != 0)
+		return half * half * 2;
+	else
+		return half * half;
+}
+
 int main()
 {
 	#ifdef DBG
 	freopen(PROBLEM TESTC ".in", "r", stdin);
-	freopen(PROBLEM ".out", "w", stdout);
+	freopen(PROBLEM "1" ".out", "w", stdout);
 	#endif
 	int size;
 
 	scanf("%d", &size);
 
-	int val[size];
-	for(int i = 0; i < size && scanf("%d", &val[i]) != EOF; i++);
+	for(int i = 1; i <= size && scanf("%d", &in[i]) != EOF; i++);
 
+	// cout << size << '\n';
 	// Build the segment tree
-	int segTree[size * 4];
-	createSegTree(segTree, size, val);
+	buildSeg(1, 1, size);
 
+	// for(int i = 1; i < pow(ceil(log2((double)size))+2); i++)
+		// printf("%d ", t[i]);
+	// puts("");
 	// for(int i = 0; i < size*4; i++)
 	// 	printf("%d ", segTree[i]);
 
@@ -31,8 +49,7 @@ int main()
 	int l, r;
 	while(scanf("%d %d", &l, &r) != EOF && (l && r))
 	{
-		l--, r--;
-		printf("%d\n", querySegTree(segTree, 1, 0, size-1, l, r));
+		printf("%d\n", query(1, 1, size, l, r));
 	}
 
 	return 0;
