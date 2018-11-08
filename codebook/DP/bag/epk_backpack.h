2c2d9af246068f985b89ad446adc361ca66e9792
roy4801 <a82611141@gmail.com>
2018-11-08 12:45:07 +0800

    [codebook] bag/epk_backpack.h -> DP/bag/epk_backpack.h

--- codebook/DP/bag/epk_backpack.h
+++ codebook/DP/bag/epk_backpack.h
@@ -4,7 +4,6 @@
  * Team: FJU_ElPsyCongroo
  * ver 0.0.1
  */
-
 #include <bits/stdc++.h>
 
 #define W 1000         // 背包最重 W
@@ -14,6 +13,7 @@ int weight[N];         //物品重量
 int value[N];          //物品價值
 int bag[W][2];
 
+// 0/1 背包
 void ZeroOne(){
 
 	memset(bag,0,sizeof(bag));
@@ -26,13 +26,11 @@ void ZeroOne(){
 		
 		for(int j = 0 ; j < W ; j++ )
 			bag[j][0] = bag[j][1];
-
 	}
-
 }
 
 //----------------------------------------------------------------------------------------------------
-
+// 無限背包
 void Unlimited(){
 
 	memset(bag,0,sizeof(bag));
@@ -45,12 +43,11 @@ void Unlimited(){
 		
 		for(int j = 0 ; j < W ; j++ )
 			bag[j][0] = bag[j][1];
-
 	}
 }
 
 //----------------------------------------------------------------------------------------------------
-
+// 分組背包
 int group;             //有幾組
 int how_many;          //一組幾個
 int WEIGHT,VALUE;
@@ -71,20 +68,16 @@ void Grouping(){
 					bag[j][1] = max( bag[j][1] , bag[j][0] );
 					bag[j][1] = max( bag[j][1] ,bag[j-WEIGHT][0] + VALUE );
 				}
-
 			}
-
 		}
 		
 		for(int j = 0 ; j < W ; j++ )
 			bag[j][0] = bag[j][1];
-
 	}
-
 }
 
 //----------------------------------------------------------------------------------------------------
-
+// 多重背包
 int limit[N];          //物品上限
 
 void Multiple(){
@@ -116,7 +109,5 @@ void Multiple(){
 				tmp = tmp*2;
 				
 		}
-
 	}
-
 }
