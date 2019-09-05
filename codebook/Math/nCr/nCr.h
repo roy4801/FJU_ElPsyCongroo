fa58ec10609dd9d7307d9eafb26ac322fcddd2d2
roy4801 <a82611141@gmail.com>
2019-09-05 23:53:40 +0800

    [codebook] Add nCr

--- codebook/Math/nCr/nCr.h
+++ codebook/Math/nCr/nCr.h
@@ -1,17 +1,12 @@
-#include <bits/stdc++.h>
-
-using namespace std;
 #define MAXN 100
-
 long long nCr[MAXN+5][MAXN+5];
-
 void build_nCr(){ //nCr[i][j] = C n 取 r
-	for(int i = 1 ; i < MAXN+5 ; i++ ){
-		for(int j = 1 ; j < MAXN+5 ; j++ ){
-			if( i == j )
-				nCr[i][j] = 1;
-			else if( i > j )
-				nCr[i][j] = nCr[i-1][j]*i/(i-j);
-		}
-	}
+    for(int i = 1 ; i < MAXN+5 ; i++ ){
+        for(int j = 1 ; j < MAXN+5 ; j++ ){
+            if( i == j )
+                nCr[i][j] = 1;
+            else if( i > j )
+                nCr[i][j] = nCr[i-1][j]*i/(i-j);
+        }
+    }
 }
