4eea50e43f44dcd6aed7b36ddc6478a9856cd6c0
roy4801 <a82611141@gmail.com>
2019-09-27 19:34:59 +0800

    Add members and ch template

--- codebook/Basic/template.cpp
+++ codebook/Basic/template.cpp
@@ -5,13 +5,19 @@ using namespace std;
 #define TESTC "{2}"
 
 #define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
+typedef long long int LL;
+typedef unsigned long long ULL;
 typedef pair<int, int> P;
+typedef pair<LL, LL> PLL;
 #define F first
 #define S second
 #define INF 0x3f3f3f3f
 #define MP make_pair
+#define MT make_tuple
 #define PB push_back
-#define POPB pop_back
+#define PPB pop_back
+#define PF push_front
+#define PPF pop_front
 
 int main()
 {
