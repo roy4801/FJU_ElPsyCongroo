81e5daa16bfee317de8e73e11d74f155566f3a36
roy4801 <a82611141@gmail.com>
2019-11-18 19:26:56 +0800

    Update scripts

--- script/cp.sh
+++ script/cp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 clear
-
 g++ $1.cpp -DDBG -o $1
-
 if [[ "$?"=="0" ]]; then
 	echo Running
-	./$1
+	./$1 < $1.in > $1$2.out
+	echo END
 fi
 
 	
--- script/new.sh
+++ script/new.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 clear
-
-cat template | sed -e "s/PROBLEM \"\"/PROBLEM \"$1\"/g" > $1.cpp
-touch $1.in
-touch $1.out
-
+cat template > $1.cpp
+touch $1.in $1.out
 echo $1 Created
