ac59285d89b632e9baa835475abca6bd7a3c5570
roy4801 <a82611141@gmail.com>
2019-11-22 20:53:17 +0800

    bitset

--- codebook/STL/bitset.cpp
+++ codebook/STL/bitset.cpp
@@ -1,25 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
+bitset<40> b;//建10格大小的bitset
+int k = 20;
+bitset<40> bk(k);//將int直接轉為bitset
+string str = "10100110";
+bitset<40> bstr(str);//將string直接轉為bitset
+// bstr[0] = 1, bstr[1] = 0, bstr[2] = 1, ... , bstr[6] = 1, bstr[7] = 0;
 
-int main(){
+b.reset();//每個位元設0
+b.set();//每個位元設1
+b[pos] = 1;//第pos個位元設成1
+b.count();//有幾個1
+b.size();//大小
 
-    bitset<40> b;//建10格大小的bitset
-    int k = 20;
-    bitset<40> bk(k);//將int直接轉為bitset
-    string str = "10100110"
-    bitset<40> bstr(str);//將string直接轉為bitset
-    // bstr[0] = 1, bstr[1] = 0, bstr[2] = 1, ... , bstr[6] = 1, bstr[7] = 0;
-
-    b.reset();//每個位元設0
-    b.set();//每個位元設1
-    b[pos] = 1;//第pos個位元設成1
-    b.count();//有幾個1
-    b.size();//大小
-
-    cin >> b;
-    //input: "0110xx0011"
-    //b = "0110"
-    bitset<8> bb(string("1001"));
-    cout << bb ;// 00001001
-
-}
+cin >> b;
+//input: "0110xx0011"
+//b = "0110"
+bitset<8> bb("1001");
+cout << bb ;// 00001001
