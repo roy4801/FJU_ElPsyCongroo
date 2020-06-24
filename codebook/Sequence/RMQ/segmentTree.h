91bfad0f5c8d492c905c128c5c3ceefd5fda2a1b
roy4801 <a82611141@gmail.com>
2020-06-24 17:46:00 +0800

    Update segmentTree.h

--- codebook/Sequence/RMQ/segmentTree.h
+++ codebook/Sequence/RMQ/segmentTree.h
@@ -1,50 +1,49 @@
-#define N 10000
-// 1-index
-int t[4*N+5];
-int in[N+5];
-
+#define N 100000
 #define LEFT(x) ((x)<<1)
 #define RIGHT(x) (((x)<<1)+1)
+// 1-idx
+int t[4*N+5];
+int in[N+5];
 // parent, left, right
-void buildSeg(int p, int inL, int inR)
+void build(int p, int L, int R)
 {
-  if(inL == inR) {
-    t[p] = in[inL];
-    return;
-  }
-  int mid = (inL+inR)/2;
-  buildSeg(LEFT(p), inL, mid);   // build left subtree
-  buildSeg(RIGHT(p), mid+1, inR);// build right subtree
-  t[p] = max(t[LEFT(p)], t[RIGHT(p)]);
+	if(L == R)
+	{
+		t[p] = in[L];
+		return;
+	}
+	int mid = (L+R)/2;
+	build(LEFT(p), L, mid);
+	build(RIGHT(p), mid+1, R);
+	t[p] = max(t[LEFT(p)], t[RIGHT(p)]);
 }
 // treeIdx, left, right, targetIdx, tragetVal
 void modify(int p, int L, int R, int i, int x)
 {
-  // stop point
-  if(i == L && L == R) {
-    t[p] = x;
-    return;
-  }
-  int mid = (L+R) / 2;
-  if(i <= mid)
-    modify(LEFT(p), L, mid, i, x);
-  else
-    modify(RIGHT(p), mid+1, R, i, x);
-  // update this node
-  t[p] = max(t[LEFT(p)], t[RIGHT(p)]);
+	if(i == L && L == R)
+	{
+		t[p] = x;
+		return;
+	}
+	int mid = (L+R) / 2;
+	// modify left subtree
+	if(i <= mid)
+		modify(LEFT(p), L, mid, i, x);
+	else
+		modify(RIGHT(p), mid+1, R, i, x);
+	// merge
+	t[p] = max(t[LEFT(p)], t[RIGHT(p)]);
 }
 // treeIdx, left, right, queryleft, queryright
 int query(int p, int L, int R, int quL, int quR)
 {
-  if(quL <= L && R <= quR) {
-    return t[p];
-  }
-  int mid = (L+R) / 2;
-  if(quR <= mid) // left
-    return query(LEFT(p), L, mid, quL, quR);
-  else if(mid < quL) // right
-    return query(RIGHT(p), mid+1, R, quL, quR);
-  else // middle
-    return max(query(LEFT(p), L, mid, quL, quR),
-           query(RIGHT(p), mid+1, R, quL, quR));
+	if(quL <= L && R <= quR)
+		return t[p];
+	int mid = (L+R)/2;
+	if(quR <= mid) // left
+		return query(LEFT(p), L, mid, quL, quR);
+	else if(mid < quL) // right
+		return query(RIGHT(p), mid+1, R, quL, quR);
+	else // mid
+		return max(query(LEFT(p), L, mid, quL, quR), query(RIGHT(p), mid+1, R, quL, quR));
 }
